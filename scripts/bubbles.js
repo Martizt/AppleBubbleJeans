2f928562750ee509f326bf04bfd91210428dcd7a
Josh <joshua.boycehyland15@gmail.com>
2025-01-25 11:13:05 +0000

    Looking at code

--- scripts/bubbles.js
+++ scripts/bubbles.js
@@ -1,2 +1,3 @@
 console.log("Bubbles.js script loaded");
 //All functions with the characters should go here, such as character appearing, changing assets, talking, reacting. etc.
+
--- scripts/dialogue.js
+++ scripts/dialogue.js
@@ -1,2 +1,45 @@
 console.log("Dialogue.js script loaded");
 //All things dialogue related go here, we'll see how that goes.
+
+
+
+
+function loadDialogue()
+{
+
+    switch( background)
+    {
+
+    }
+}
+
+
+
+
+let entrydialgioue = 
+[
+    "", 
+    "", 
+    "", 
+    "", 
+    ""
+]
+
+
+let midDialgioue = 
+[
+    "", 
+    "", 
+    "", 
+    "", 
+    ""
+]
+
+let exitdialgioue = 
+[
+    "", 
+    "", 
+    "", 
+    "", 
+    ""
+]
